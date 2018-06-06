e74f43ef48287d80efad06f6d57bd118c4f52567
GaponVasa <gapon.vasa@ukr.net>
2018-06-06 19:50:15 +0300

    add one change

--- js/main.js
+++ js/main.js
@@ -6,12 +6,10 @@ let field = document.querySelector('.field');
 let numberWords  = document.querySelector('.numberWords');
 
 buttonGenerate.addEventListener('click',function(){
-	// field.innerHTML = ' ';
 	field.innerHTML = loremGenerator.start(loremTable, parseInt(numberWords.value));
 });
 
 buttonReset.addEventListener('click',function(){
-	// field.innerHTML = ' ';
 	numberWords.value = '';
 	field.innerHTML = 'Lorem text.';
 })
