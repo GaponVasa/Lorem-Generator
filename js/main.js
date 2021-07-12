696bbb8bbcd72cad6ff7116f5846c7a9a679701c
GaponVasa <gapon.vasa@ukr.net>
2021-07-12 20:34:41 +0300

    fix one bug. Rename some variables.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 "use strict";
 
-let buttonGenerate = document.getElementById('btn-generate');
-let buttonReset = document.getElementById('btn-reset');
-let field = document.querySelector('.field');
-let numberWords  = document.querySelector('.numberWords');
+const buttonGenerate = document.getElementById("btn-generate");
+const buttonReset = document.getElementById("btn-reset");
+const field = document.querySelector(".field");
+const numberWords = document.querySelector(".numberWords");
 
-buttonGenerate.addEventListener('click',function(){
-	field.innerHTML = loremGenerator.start(loremTable, parseInt(numberWords.value));
+buttonGenerate.addEventListener("click", function () {
+  field.innerHTML = loremGenerator.start(
+    loremTable,
+    parseInt(numberWords.value)
+  );
 });
 
-buttonReset.addEventListener('click',function(){
-	numberWords.value = '';
-	field.innerHTML = 'Lorem text.';
-})
-
+buttonReset.addEventListener("click", function () {
+  numberWords.value = "";
+  field.innerHTML = "Lorem text.";
+});
