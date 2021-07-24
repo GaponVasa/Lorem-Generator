3e0c9c5e066af5ddae232d641691302d1777fb3e
GaponVasa <gapon.vasa@ukr.net>
2021-07-24 09:13:19 +0300

    add validation for input. add focus and blur functions for input.

--- js/main.js
+++ js/main.js
@@ -4,15 +4,33 @@ const buttonGenerate = document.getElementById("btn-generate");
 const buttonReset = document.getElementById("btn-reset");
 const field = document.querySelector(".field");
 const numberWords = document.querySelector(".numberWords");
+const inputPlaceholder = numberWords.placeholder;
+const regular = /^\d+$/gm; //тільки цифри
 
-buttonGenerate.addEventListener("click", function () {
-  field.innerHTML = loremGenerator.start(
-    loremTable,
-    parseInt(numberWords.value)
-  );
-});
+buttonGenerate.addEventListener("click", inputValidation);
 
 buttonReset.addEventListener("click", function () {
   numberWords.value = "";
   field.innerHTML = "Lorem text.";
+});
+
+//2. додати кнопку скопіювати у буфер
+
+function inputValidation() {
+  const valueInput = numberWords.value;
+  if (valueInput !== "" && regular.test(valueInput)) {
+    numberWords.classList.remove("placeholder_red");
+    field.innerHTML = loremGenerator.start(loremTable, parseInt(valueInput));
+  } else {
+    numberWords.classList.add("placeholder_red");
+  }
+  numberWords.value = "";
+}
+
+numberWords.addEventListener("focus", () => {
+  numberWords.placeholder = "";
+});
+
+numberWords.addEventListener("blur", () => {
+  numberWords.placeholder = inputPlaceholder;
 });
